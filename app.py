800a6dd0f69d5f873dd11c322bbc76638f38a68d
hbruce11216 <68431949+hbruce11216@users.noreply.github.com>
2021-12-05 22:44:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -194,4 +194,6 @@ app.layout = html.Div(
 )
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8050)
+    app.run_server(debug=True, port=8050, host='0.0.0.0')
+    # app.run_server(debug=True, port=8050)
+    
